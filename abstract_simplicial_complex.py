639cec3a2984ca414227fd6b6b3d8753633ffc45
senchromatic <samuelfzhang@gmail.com>
2021-02-15 18:42:21 -0800

    Basic library for common distance metrics, extended set operations, simplices and simplicial complexes

---
+++ abstract_simplicial_complex.py
@@ -0,0 +1,127 @@
+# Defines a Point, Simplex, and ASC (abstract simplicial complex).
+
+import metrics
+
+from functools import total_ordering
+from sets import powerset
+
+
+# A point must have a unique identifier or a (partial) coordinate vector as representation.
+# If no metric is provided, then the discrete metric will be used by default.
+@total_ordering
+class Point:
+  
+  def __init__(self, name=None, coordinates=None, dist_metric=None, show_coords=False):
+    if name is None and coordinates is None:
+      raise ValueError
+    self.name = name
+    self.coordinates = coordinates
+    self.dist_metric = dist_metric if dist_metric else metrics.discrete_metric
+    self.show_coords = show_coords
+  
+  # Formats:
+  #   [name]([coordinates])  -- if both exist
+  #   [name]                 -- only name
+  #   (coordinates)          -- only coordinates
+  def __str__(self):
+    s = ""
+    if self.name:
+      s += self.name
+    if self.show_coords and self.coordinates is not None:
+      s += '(' + ' '.join([str(x) for x in self.coordinates]) + ')'
+    return s
+  
+  def distance_to(self, other_point):
+    return self.dist_metric(self.coordinates, other_point.coordinates)
+  
+  # Compare (approximately) by coordinates.
+  def similar_to(self, other_point):
+    return self.distance_to(other_point) <= metrics.DEFAULT_EPS
+  
+  # If both name and coordinates are provided, and the metrics are equal, these two points are indistinguishable.
+  # Otherwise, compare by the arguments provided.
+  def __eq__(self, other):
+    have_names = not(self.name is None or other.name is None)
+    metric_comparable = not(self.coordinates is None or other.coordinates is None) and self.dist_metric == other.dist_metric
+    same_names = self.name == other.name
+    close_enough = self.similar_to(other)
+    if have_names and metric_comparable:
+      return same_names and close_enough
+    if have_names:
+      return same_names
+    if metric_comparable:
+      return close_enough
+    raise ValueError("Unable to compare", self, "and", other)
+  
+  def __lt__(self, other):
+    if not(self.name is None or other.name is None):
+      return self.name < other.name
+    return metrics.norm(self.coordinates) < metrics.norm(other.coordinates)
+  
+  def __hash__(self):
+    return hash(self.name)
+  
+# A k-simplex consists of (d+1) points.
+@total_ordering
+class Simplex:
+  def __init__(self, points=None):
+    self.points = points if points else set()
+
+  def dimension(self):
+    return len(self.points) - 1
+  
+  def __str__(self, sep=', '):
+    return '{' + sep.join(sorted(map(str, self.points))) + '}'
+  
+  def add_point(self, other_point):
+    self.points.add(other_point)
+  
+  def add_points(self, other_simplex):
+    self.points = self.points | other_simplex.points
+  
+  def get_point_by_name(self, name):
+    for p in self.points:
+      if p.name == name:
+        return p
+    return None
+  
+  def __lt__(self, other):
+    return sorted(list(self.points)) < sorted(list(other.points))
+  
+  def __eq__(self, other):
+    return self.points == other.points
+  
+  def __hash__(self):
+    return hash('|'.join(map(str,self.points)))
+  
+# An abstract simplicial complex is a collection of simplices closed under the subset operation.
+class ASC:
+  def __init__(self, simplices=None):
+    self.simplices = simplices if simplices else set()
+  
+  # We define the (highest) "dimension" of an ASC as the maximum dimension among its simplices, if -1 if it's empty.
+  def highest_dimension(self):
+    return max([sim.dimension() for sim in self.simplices]) if self.simplicies else -1
+  
+  def all_dimensions(self):
+    return set([sim.dimension() for sim in self.simplices])
+  
+  # All simplices of dimesion k, for a given k
+  def k_simplices(self, k):
+    return set(filter(lambda sim : sim.dimension() == k, self.simplices))
+  
+  # String representation of simplices sorted by (non-decreasing) dimension
+  def __str__(self, sep="\n\n"):
+    return '{' + sep.join(['; '.join(map(str, sorted(self.k_simplices(k)))) for k in self.all_dimensions()]) + '}'
+  
+  # Check closure by taking all subsets.
+  def is_valid(self):
+    for subset in powerset(self.simplices):
+      sim = Simplex(subset)
+      if sim not in self.simplices:
+        return False
+    return True
+  
+  def add_simplex(self, sim):
+    self.simplices.add(sim)
+
---
+++ hw1_example.py
@@ -0,0 +1,104 @@
+# A biological taxonomy on living things provides us with three key characteristics in this sample -
+# Coordinate 1 (biosphere): 0 for hydosphere (water), 1 biosphere (land)
+# Coordinate 2 (kingdom): 0 for plantae (plants), 1 for animalia (animals)
+# The other dimensions used to distinguish each "species" are encoded as a string (sequence of characters), its name.
+
+from abstract_simplicial_complex import Point, Simplex, ASC
+from enum import Enum
+
+import numpy as np
+
+# Organisms are organic entities.
+organisms = Simplex()
+
+def create_organisms():
+  # Terrestrial animals:
+  for name in ["Cow", "Rabbit", "Horse", "Dog"]:
+    organisms.add_point(Point(name, np.array([1,1])))
+  # Aquatic animals
+  for name in ["Fish", "Dolphin", "Oyster"]:
+    organisms.add_point(Point(name, np.array([0,1])))
+  # Plants
+  for name in ["Broccoli", "Fern", "Onion", "Apple"]:
+    organisms.add_point(Point(name, np.array([1,0])))
+
+def get_organism(name):
+  return organisms.get_point_by_name(name)
+
+def point_comparison_test():
+  cow = get_organism("Cow")
+  rabbit = get_organism("Rabbit")
+  dolphin = get_organism("Dolphin")
+  apple = get_organism("Apple")
+  print("Here is a land animal:", cow)
+  print("Here is an aquatic animal:", dolphin)
+  print("Here is a land plant:", apple)
+  print("Is the cow the same as a rabbit?", cow == rabbit)
+  print("Is the cow similar to a rabbit?", cow.similar_to(rabbit))
+  print("Is the rabbit similar to a dolphin?", rabbit.similar_to(dolphin))
+  print("Is the dolphin similar to an apple?", dolphin.similar_to(apple))
+
+# Create all k-simplices for a given metric and distance threshold
+def cech_complex(points, k):
+  pass  # TODO
+
+# Create all k-simplices for a given metric and distance threshold
+def vietoris_rips_complex(points, k):
+  pass  # TODO
+
+if __name__ == '__main__':
+  # Point cloud
+  create_organisms()
+  # print("All organisms:", organisms)
+  # point_comparison_test()
+  
+  
+  asc1 = ASC()
+  print("------------- Abstract Simplicial Complex A -------------")
+  
+  # Generate points in ASC
+  for p in organisms.points:
+    asc1.add_simplex(Simplex({p}))
+  print("Number of 0-simplices:", len(asc1.k_simplices(0)))
+  
+  # Generate line segments in ASC
+  for p1 in organisms.points:
+    for p2 in organisms.points - {p1}:
+      if p1.similar_to(p2):
+        new_simplex = Simplex({p1, p2})
+        asc1.add_simplex(new_simplex)
+  print("Number of 1-simplices:", len(asc1.k_simplices(1)))
+  
+  # Generate triangles in ASC
+  for p1 in organisms.points:
+    for p2 in organisms.points - {p1}:
+      for p3 in organisms.points - {p1, p2}:
+        if p1.similar_to(p2) and p1.similar_to(p3) and p2.similar_to(p3):
+          new_simplex = Simplex({p1, p2, p3})
+          asc1.add_simplex(new_simplex)
+  print("Number of 2-simplices:", len(asc1.k_simplices(2)))
+  
+  print(asc1)
+  print("\n\n")
+  
+  asc2 = ASC()
+  print("------------- Abstract Simplicial Complex B -------------")
+  
+  # Generate points in ASC
+  for p in organisms.points:
+    asc2.add_simplex(Simplex({p}))
+  print("Number of 0-simplices:", len(asc2.k_simplices(0)))
+  
+  # Add all line segments to ASC
+  for p1,p2 in {("Cow", "Rabbit"), ("Cow", "Fish"), ("Cow", "Oyster"), ("Cow", "Oyster"), ("Cow", "Broccoli"), ("Cow", "Onion"), ("Cow", "Apple"), ("Rabbit", "Fish"), ("Rabbit", "Oyster"), ("Rabbit", "Broccoli"), ("Rabbit", "Onion"), ("Rabbit", "Apple"), ("Fish", "Oyster"), ("Fish", "Broccoli"), ("Fish", "Onion"), ("Fish", "Apple"), ("Oyster", "Broccoli"), ("Oyster", "Onion"), ("Oyster", "Apple"), ("Broccoli", "Onion"), ("Broccoli", "Apple"), ("Onion", "Apple"), ("Horse", "Dog"), ("Horse", "Dolphin"), ("Horse", "Fern"), ("Dog", "Dolphin"), ("Dog", "Fern"), ("Dolphin", "Fern")}:
+    asc2.add_simplex(Simplex({get_organism(p1), get_organism(p2)}))
+  print("Number of 1-simplices:", len(asc2.k_simplices(1)))
+    
+  # Add all triangles to ASC
+  for p1,p2,p3 in {("Cow", "Broccoli", "Apple"), ("Cow", "Onion", "Apple"), ("Rabbit", "Broccoli", "Apple"), ("Rabbit", "Onion", "Apple"), ("Fish", "Broccoli", "Apple"), ("Fish", "Onion", "Apple"), ("Oyster", "Broccoli", "Apple"), ("Oyster", "Onion", "Apple")}:
+    asc2.add_simplex(Simplex({get_organism(p1), get_organism(p2), get_organism(p3)}))
+  print("Number of 2-simplices:", len(asc2.k_simplices(2)))
+  
+  print(asc2)
+  print("\n\n")
+
